2b98ebccba2df229e2c3e9f1d4c9a7e9f4fa6c52
PinBib <105994863+Pinbib@users.noreply.github.com>
2022-11-29 16:58:01 +0000

    ok

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 var pages = document.querySelectorAll('.page');
 var pagesList = ['SCRIMER'];
 pages[0].onclick = (event)=>{
-    var scrimer = document.createElement('a');
-    scrimer.href = 'https://pinbib.github.io/SCRIMER/index.html';
-    scrimer.target = '_blank';
-    scrimer.innerHTML = 'click';
-    pages[0].innerHTML = scrimer;
+    pages[0].innerHTML = '<a href=\'https://pinbib.github.io/SCRIMER/index.html\' target=\'_blank\'>click</a>';
 }
