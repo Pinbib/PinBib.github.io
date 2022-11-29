0e21a589cb0370b15cc8f6fa8642349dedb4cd6c
PinBib <105994863+Pinbib@users.noreply.github.com>
2022-11-29 16:25:47 +0000

    OK

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var i = 0;
 while(i<pagesL){
     if(pages[i]==pagesList[i]){
         var scrimer = document.createElement('a');
-        scrimer.href = '/workspaces/PinBib.github.io/SCRIMER/index.html';;
+        scrimer.href = '/PinBib.github.io/SCRIMER/index.html';;
         scrimer.target = '_blank';
         scrimer.click();
     }
